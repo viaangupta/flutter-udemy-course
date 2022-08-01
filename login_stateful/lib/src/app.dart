9c5b0725f8791443e5268c6ebe15e93a03ada90b
Ajeet Gupta <ajeetkg@bu.edu>
2022-08-01 06:20:48 -0700

    basic app started

---
+++ login_stateful/lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget{
+  Widget build(context) {
+    return MaterialApp(
+      title: 'Log me In',
+      home: Scaffold (
+        body: Text('Show me a form here!'),
+      )
+    );
+  }
+}
